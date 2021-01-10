3a14d2e43b6008c9b6b15e900f229f4d3a7bc5da
vishnupavan1 <37592221+vishnupavan1@users.noreply.github.com>
2021-01-10 17:41:45 +0530

    Update driver.scala

--- spark/src/main/scala/com/bigdata/ex/driver.scala
+++ spark/src/main/scala/com/bigdata/ex/driver.scala
@@ -4,10 +4,27 @@ object driver {
 
   def main(args: Array[String]): Unit = {
 
-    /* Setting system properties and log level*/
+    /* Setting system properties */
     System.setProperty("hadoop.home.dir", "C:\\hadoop")
+    
+    /*
+     * creating an object to intiate the process 
+     * 
+     * This sample project has two methods 
+     * 1.Initialize --> This will create the spark session --> Entry point for spark execution 
+     * 2.Process --> This will start the process and run the instructions 
+     * 
+     * we can use property file --> which will contain the process name , and class name to run we can read the property file
+     * and parse the arguments and trigger the code for different data sources
+     * 
+     *  */
 
     val process_obj = Class.forName("com.bigdata.ex.Sample").newInstance().asInstanceOf[com.bigdata.ex.Transformation]
+    
+    /*
+     * 
+     * 
+     * */
     
     process_obj.initialize()
     process_obj.process()
